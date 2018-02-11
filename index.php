f3cb4ddb15707058edd4626c07f3f5aa3e425ed4
Денис Бирченко <mariiamuratova96@gmail.com>
2018-02-11 03:34:43 +0300

    OOP

--- index.php
+++ index.php
@@ -3,27 +3,65 @@ class  Car {
 
     public $brand = 123;
     public $model;
-    public $price;
+    private $price;
+    private $state = 'static';
 
+    public function __construct($brand, $model)
+    {
+        $this->brand = $brand;
+        $this->model = $model;
+
+    }
+
+    public function __destruct()
+    {
+
+    }
+
+   // function test() {
+   //     $this->brand;
+    //}
     function drive()
     {
-        echo $this->drive;
+       $this->state = 'moving';
     }
     function stop()
     {
-        echo 'Stop';
+        if ($this->isMoving()) {
+            echo 'You are not moving';
+        } else {
+            $this->state = 'static';
+        }
+    }
+    function isMoving()
+    {
+        return $this->state != 'moving';
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        if(!is_numeric($price)){
+            die('Invalid price');
+        }
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
     }
 }
-$car1 = new Car();
-$car1 -> brand = 'Mazda';
-$car1 ->model ='6';
-$car1 -> drive();
+$car1 = new Car('Mazda', 6);
+$car1->setPrice(1000);
 
-$car2 = new Car();
 
-$car2 -> brand = 'BMW';
-$car2 ->model = 'x6';
-$car2 ->price = 2000;
+$car2 = new Car('BMW', 6);
 var_dump($car1, $car2);
 
 ?>
